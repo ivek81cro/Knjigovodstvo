e0a73a888874dff90177224ce6df4719cc298e66
Ivan Batinić <ivek81cro@gmail.com>
2020-10-14 23:57:00 +0200

    Sreate settings class for app

---
+++ Knjigovodstvo/Models/Postavke.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Knjigovodstvo.Models
+{
+    class Postavke
+    {
+        public float Porez1 { get; set; }
+        public float Porez2 { get; set; }
+        public float Porez3 { get; set; }
+        public float Porez4 { get; set; }
+    }
+}
