ec2d7813169f33210bbfca13e994433f1e81c5af
Ivan Batinić <ivek81cro@gmail.com>
2020-10-21 21:33:04 +0200

    Add validation for float

---
+++ Knjigovodstvo/Validators/FloatValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Knjigovodstvo.Validators
+{
+    class FloatValidator
+    {
+        public bool Check(string f)
+        {
+            if (float.TryParse(f, out _))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
