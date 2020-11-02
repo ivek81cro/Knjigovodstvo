230bdb0ce0b474c80ceae3bc4ad888b66f9a5ce7
Ivan Batinić <ivek81cro@gmail.com>
2020-11-02 23:30:13 +0100

    Extract method to other class

--- Knjigovodstvo/JoppdDocument/JoppdPlacaForm.cs
+++ Knjigovodstvo/JoppdDocument/JoppdPlacaForm.cs
@@ -132,171 +132,11 @@ namespace Knjigovodstvo.JoppdDocument
 
                                            }).ToList();
 
-            List<sPrimateljiP> pArr = new List<sPrimateljiP>();
-            for (int i = 0; i < _joppdEntiteti.JoppdEntitet.Count; i++)
-            {
-                JoppdEntitet e = _joppdEntiteti.JoppdEntitet[i];
-                e.PopuniDodatke(i + 1);
-                pArr.Add(new sPrimateljiP()
-                {
-                    P1 = e.Redni_Broj,
-                    P2 = e.Opcina_Prebivalista,
-                    P3 = e.Opcina_Rada,
-                    P4 = e.Oib,
-                    P5 = e.Ime_Prezime,
-                    P61 = (tOznakaStjecatelja)Enum.Parse(typeof(tOznakaStjecatelja), "Item" + e.Stjecatelj),
-                    P62 = (tOznakaPrimici)Enum.Parse(typeof(tOznakaPrimici), "Item" + e.Primitak),
-                    P71 = (tOznakaMO)Enum.Parse(typeof(tOznakaMO), e.Beneficirani),
-                    P72 = (tOznakaInvaliditet)Enum.Parse(typeof(tOznakaInvaliditet), "Item" + e.Invaliditet),
-                    P8 = (tOznakaMjesec)Enum.Parse(typeof(tOznakaMjesec), "Item" + e.Mjesec),
-                    P9 = (tOznakaRadnoVrijeme)Enum.Parse(typeof(tOznakaRadnoVrijeme), "Item" + e.Vrijeme),
-                    P10 = e.Sati,
-                    P101 = Convert.ToDateTime(e.Datum_Od),
-                    P102 = Convert.ToDateTime(e.Datum_Do),
-                    P11 = e.Bruto,
-                    P12 = e.Bruto,
-                    P121 = e.Mio_1,
-                    P122 = e.Mio_2,
-                    P123 = e.Zdravstvo,
-                    P124 = e.Zaštita,
-                    P125 = e.Zaposljavanje,
-                    P126 = e.Povecani_Mio,
-                    P127 = e.Povecani_Mio2,
-                    P129 = e.Invaliditet_Doprinos,
-                    P131 = e.Izdatak,
-                    P132 = e.IzdatakUplaceni_Mio,
-                    P133 = e.Dohodak,
-                    P134 = e.Osobni_Odbitak,
-                    P135 = e.Porezna_Osnovica,
-                    P141 = e.Porez_Ukupno,
-                    P142 = e.Prirez,
-                    P151 = (tOznakaNeoporezivogPrimitka)Enum.Parse(typeof(tOznakaNeoporezivogPrimitka), e.Oznaka_Neoporezivog),
-                    P152 = e.Iznos_Neoporezivog,
-                    P161 = (tOznakaNacinaIsplate)Enum.Parse(typeof(tOznakaNacinaIsplate), e.Nacin_Isplate),
-                    P162 = e.Iznos_Isplate,
-                    P17 = e.Primitak_Nesamostalni
-                });
-            }
-            JoppdA jA = new JoppdA(pArr);
-            sStranaA strA = new sStranaA()
-            {
-                DatumIzvjesca = dateTimePicker1.Value,
-                OznakaIzvjesca = SetJoppdFormNumber(),
-                VrstaIzvjesca = tVrstaIzvjesca.Item2,
-                IzvjesceSastavio = new sIzvjesceSastavio()
-                {
-                    Ime = textBoxIzvjesceSastavioIme.Text.Split(' ')[0],
-                    Prezime = textBoxIzvjesceSastavioIme.Text.Split(' ')[1]
-                },
-                PodnositeljIzvjesca = new sPodnositeljIzvjesca()
-                {
-                    ItemsElementName = new[] { ItemsChoiceType.Naziv },
-                    Items = new[] { _komitent.Naziv },
-                    Adresa = new sAdresa()
-                    {
-                        Ulica = _komitent.Adresa.Split(' ')[0],
-                        Broj = _komitent.Adresa.Split(' ')[1],
-                        Mjesto = _komitent.Grad
-                    },
-                    Email = _komitent.Mail,
-                    OIB = _komitent.Oib,
-                    Oznaka = tOznakaPodnositelja.Item2
-                },
-                BrojOsoba = pArr.Count.ToString(),
-                BrojRedaka = pArr.Count.ToString(),
-                PredujamPoreza = new sPredujamPoreza()
-                {
-                    P1 = jA.UkupnoPorezIPrirez(),
-                    P11 = jA.UkupnoPorezIPrirez(),
-                    P12 = 0,
-                    P2 = 0,
-                    P3 = 0,
-                    P4 = 0,
-                    P5 = 0,
-                    P6 = 0
-                },
-                Doprinosi = new sDoprinosi()
-                {
-                    GeneracijskaSolidarnost = new sGeneracijskaSolidarnost()
-                    {
-                        P1 = jA.SviDoprinosiGeneracijskaDjelatnici(),
-                        P1Specified = true,
-                        P2 = 0,
-                        P3 = jA.SviDoprinosiGeneracijskaPoduzetnik(),
-                        P3Specified = true,
-                        P4 = 0,
-                        P5 = 0,
-                        P6 = 0,
-                        P7 = 0
-                    },
-                    KapitaliziranaStednja = new sKapitaliziranaStednja()
-                    {
-                        P1 = jA.SviDoprinosiKapitaliziranaDjelatnici(),
-                        P1Specified = true,
-                        P2 = 0,
-                        P3 = jA.SviDoprinosiKapitaliziranaPoduzetnik(),
-                        P3Specified = true,
-                        P4 = 0,
-                        P5 = 0,
-                        P6 = 0
-                    },
-                    ZdravstvenoOsiguranje = new sZdravstvenoOsiguranje()
-                    {
-                        P1 = jA.ZdravstveoDjelatnici(),
-                        P1Specified = true,
-                        P2 = 0,
-                        P3 = jA.ZdravstvenoPoduzetnici(),
-                        P3Specified = true,
-                        P4 = 0,
-                        P5 = 0,
-                        P6 = 0,
-                        P7 = 0,
-                        P8 = 0,
-                        P9 = 0,
-                        P10 = 0,
-                        P11 = 0,
-                        P12 = 0
-                    },
-                    Zaposljavanje = new sZaposljavanje()
-                    {
-                        P1 = 0,
-                        P2 = 0,
-                        P3 = 0,
-                        P4 = 0
-                    }
-                },
-                IsplaceniNeoporeziviPrimici = jA.ZbrojNeoporezivo(),
-                IsplaceniNeoporeziviPrimiciSpecified = true,
-                KamataMO2 = 0,
-                UkupniNeoporeziviPrimici = 0,
-                NaknadaZaposljavanjeInvalida = new sNaknadaZaposljavanjeInvalida() 
-                {
-                    P1 = "0",
-                    P2 = 0
-                }
-                //TODO continue with member value assigning
-            };
-            
-            sJOPPDmetapodaci meta = new sJOPPDmetapodaci()
-            {
-                Datum = new sDatumTemeljni() { Value = dateTimePicker1.Value },
-                Naslov = new sNaslovTemeljni() { Value = "Izvješće o primicima, porezu na dohodak i prirezu te doprinosima za obvezna osiguranja" },
-                Autor = new sAutorTemeljni() { Value = "Ivan Batinić" },
-                Format = new sFormatTemeljni() { Value = tFormat.textxml },
-                Jezik = new sJezikTemeljni() { Value = tJezik.hrHR },
-                Identifikator = new sIdentifikatorTemeljni() { Value = Guid.NewGuid().ToString() },
-                Uskladjenost = new sUskladjenost() { Value = "ObrazacJOPPD-v1-1" },
-                Tip = new sTipTemeljni() { Value = tTip.Elektroničkiobrazac },
-                Adresant = new sAdresantTemeljni() { Value = "Ministarstvo Financija, Porezna uprava, Zagreb" }
-            };
-            sPrimateljiP[][] prim = { pArr.ToArray() };
+            _sObrazacJoppd = new JoppdObrazac(_joppdEntiteti, _komitent)
+                .CreateJoppdXml(dateTimePicker1.Value, SetJoppdFormNumber(), textBoxIzvjesceSastavioIme.Text);
 
-            _sObrazacJoppd.StranaA = strA;
-            _sObrazacJoppd.StranaB = prim;
-            _sObrazacJoppd.Metapodaci = meta;
-
-            string path = @"JoppdBroj" + SetJoppdFormNumber() + ".xml";
-
+            string path = @"JoppdBroj" + SetJoppdFormNumber() + ".xml"; //Replace with user input (Open file dialog)
+            //Save joppd.xml file
             System.IO.TextWriter txtWriter = new System.IO.StreamWriter(path);
             System.Xml.Serialization.XmlSerializer x = new System.Xml.Serialization.XmlSerializer(_sObrazacJoppd.GetType());
             x.Serialize(txtWriter, _sObrazacJoppd);
@@ -305,12 +145,10 @@ namespace Knjigovodstvo.JoppdDocument
             System.IO.StreamReader reader = new System.IO.StreamReader(path);
             sObrazacJOPPD enti = (sObrazacJOPPD)x.Deserialize(reader);
             reader.Close();
-
+            //Read xml file into datagridview
             DataSet dataSet = new DataSet();
             dataSet.ReadXml(path);
             dataGridView1.DataSource = dataSet.Tables[dataSet.Tables.Count-1];
-
-            enti.GetType();
         }
 
         private void TextBoxSatiRada_KeyPress(object sender, KeyPressEventArgs e)
@@ -330,7 +168,7 @@ namespace Knjigovodstvo.JoppdDocument
         private DataTable _dt = new DataTable();
         private JoppdSifre _joppdSifre = new JoppdSifre();
         private JoppdEntitetCollection _joppdEntiteti = new JoppdEntitetCollection();
-        private sObrazacJOPPD _sObrazacJoppd = new sObrazacJOPPD();
         private Komitent _komitent = new Komitent();
+        private sObrazacJOPPD _sObrazacJoppd = new sObrazacJOPPD();
     }
 }
