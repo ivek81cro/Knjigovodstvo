5b5f15c74a51519798bcd4eef7eb4bf45d4698d7
ivek81cro <ivek81cro@gmail.com>
2021-01-18 13:41:04 +0100

    Add Invetory class

---
+++ Knjigovodstvo/Books/Inventory/OsnovnoSredstvo.cs
@@ -0,0 +1,26 @@
+﻿using Knjigovodstvo.Interface;
+using System;
+
+namespace Knjigovodstvo.Books.Inventory
+{
+    class OsnovnoSredstvo : IDbObject
+    {
+        public FormError ValidateData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Id { get; set; } = 0;
+        public string Naziv { get; set; } = "";
+        public string Datum_nabave { get; set; } = "";
+        public string Datum_uporabe { get; set; } = "";
+        public string Dobavljac { get; set; } = "";
+        public string Dokument { get; set; } = "";
+        public decimal Kolicina { get; set; } = 0;
+        public decimal Nabavna_vrijednost { get; set; } = 0;
+        public decimal Vijek_trajanja { get; set; } = 0;
+        public decimal Stopa_otpisa { get; set; } = 0;
+        public decimal Otpisano { get; set; } = 0;
+        public decimal Sadasnja_vrijednost { get; set; } = 0;
+    }
+}
