49dda2885bcd04d78b82a641a1ab4d787a09c2f7
Ivan Batinić <ivek81cro@gmail.com>
2021-02-09 19:56:18 +0100

    Add DPI support

--- Knjigovodstvo/Program.cs
+++ Knjigovodstvo/Program.cs
@@ -1,11 +1,23 @@
 using Knjigovodstvo.MainForm;
 using System;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace Knjigovodstvo
 {
     static class Program
     {
+        [DllImport("Shcore.dll")]
+        static extern int SetProcessDpiAwareness(int PROCESS_DPI_AWARENESS);
+
+        // According to https://msdn.microsoft.com/en-us/library/windows/desktop/dn280512(v=vs.85).aspx
+        private enum DpiAwareness
+        {
+            None = 0,
+            SystemAware = 1,
+            PerMonitorAware = 2
+        }
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -15,6 +27,9 @@ namespace Knjigovodstvo
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            SetProcessDpiAwareness((int)DpiAwareness.PerMonitorAware);
+
             Application.Run(new MainWindowForm());
         }
     }
